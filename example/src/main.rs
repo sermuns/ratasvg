341e8016c80a2f5f173b8235861886ad65b1fd30
Samuel Åkesson <sermuns@lysator.liu.se>
2026-06-24 22:52:46 +0200

    add ratatui mascot example

--- example/src/main.rs
+++ example/src/main.rs
@@ -20,15 +20,14 @@ fn main() -> color_eyre::Result<()> {
         ),
     )?;
 
-    // https://github.com/ratatui/ratatui/blob/main/examples/vhs/release-header.tape
     svg::save(
         "ratatui-mascot.svg",
         &build_svg_from_widget(
             RatatuiMascot::new(),
             ratasvg::Options {
-                background_color: "#141432",
-                width_px: 1120,
-                height_px: 520,
+                background_color: "black",
+                width_px: 500,
+                height_px: 400,
                 font_size_px: 25,
             },
         ),
